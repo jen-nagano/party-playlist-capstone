519a597d341f3c8771a9853dbd89f0c421b00764
Jennifer Nagano <jen.nagano@gmail.com>
2024-12-02 17:10:22 -0500

    incorporated a sample search using spotify api

---
+++ vue/src/views/EventView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Event">
+      <h1>Event</h1>
+      <p>Complete the form to add a new event.</p>
+      <EventForm />
+    </div>
+  </template>
+  
+  <script>
+  import EventForm from '../components/EventForm.vue';
+
+  export default {
+    name: 'EventView',
+    components:{
+      EventForm
+    }
+  };
+  </script>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,15 +2,40 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-    <EventForm />
+    <router-link class="btn btn-submit" :to="{ name: 'event' }">Create Event</router-link>
+    <p>{{ trackInfo.name }}</p>
+    <!--<p>By: {{ trackInfo.artists[0].name }}</p> -->
+    <form v-on:submit.prevent="getTrack()" class="cardForm">
+      <div class="form-group">
+        <label for="track-id">Track Id:</label>
+        <input id="track-id" type="text" class="form-control" v-model="trackId" autocomplete="off" />
+      </div>
+      <button class="btn btn-submit">Submit</button>
+      <!--<button v-on:click="getTrack()">Get Track</button> -->
+    </form>
   </div>
 </template>
 
 <script>
-import EventForm from '../components/EventForm.vue';
+import SpotifyService from '../services/SpotifyService.js';
+
 export default {
-  components:{
-    EventForm
+
+  components: {},
+  data() {
+    return {
+      trackId:'',
+      trackInfo: {}
+    }
+  },
+  methods: {
+    getTrack() {
+      SpotifyService.getTrack(this.trackId)
+        .then(response => {
+          this.trackInfo = response.data;
+        })
+    }
   }
+
 };
 </script>

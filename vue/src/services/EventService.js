663ae11b88efa8944890c4356103607db9f41770
Jennifer Nagano <jen.nagano@gmail.com>
2024-12-02 15:39:26 -0500

    Added form to home page to test ability to post a new event

---
+++ vue/src/services/EventService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: import.meta.env.VITE_REMOTE_API
+});
+
+export default {
+
+  addEvent(event) {
+    return http.post('/events', event)
+  },
+
+  updateEvent(event) {
+    return http.put('/events', event)
+  }
+
+}
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,10 +2,15 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <EventForm />
   </div>
 </template>
 
 <script>
+import EventForm from '../components/EventForm.vue';
 export default {
+  components:{
+    EventForm
+  }
 };
 </script>
